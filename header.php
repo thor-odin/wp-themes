b6dce06d8dcf00578684f90fbc7a85ae1076f12f
Chirag Bhangale <Chirag Bhangale>
2017-02-07 06:35:35 -0500

    commit

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Triangle Freethought Society</title>
 
     <!-- Bootstrap -->
     <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
@@ -16,7 +16,27 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-      
-    <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#top-navigation">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">TFS</a>
+    </div>
+    <div>
+      <div class="collapse navbar-collapse" id="top-navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#About">About</a></li>
+          <li><a href="#Calendar">Calendar</a></li>
+          <li><a href="#Membership">Membership</a></li>
+      </div>
+    </div>
+  </div>
+</nav> 
+   
