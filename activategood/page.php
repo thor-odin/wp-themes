afc982da6f7e2bcfb2d05e8463e8e85124dc48b9
Chirag Bhangale <Chirag Bhangale>
2017-03-13 06:22:52 -0400

    some more bug fixes

--- activategood/page.php
+++ activategood/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<div class="row">
-		<div class="col-xs-8 col-xs-offset-2">		
+		<div class="col-xs-12 col-md-8 col-md-offset-2">		
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 					get_template_part( 'content', get_post_format() );
 					endwhile; else: ?>
